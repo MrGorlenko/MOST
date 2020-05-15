f3981194533acc775cedeca52abbbeb6707e79a7
MrGorlenko <52908127+MrGorlenko@users.noreply.github.com>
2020-05-15 14:39:36 +0300

    links

---
+++ scripts/links.js
@@ -0,0 +1,22 @@
+ $(document).ready(function(){
+	$("#header-container").on("click","a", function (event) {
+        event.preventDefault();
+        
+		var id  = $(this).attr('href'),
+            top = $(id).offset().top;		
+            
+		$('body,html').animate({scrollTop: top}, 1500);
+    });
+    
+    $("#menu-container").on("click","a", function (event) {
+
+        $('.menu').removeClass('left-0');
+        
+        event.preventDefault();
+        
+		var id  = $(this).attr('href'),
+            top = $(id).offset().top;		
+            
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+});
