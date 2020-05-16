a871da210929a0892293bf9eab04a6e89cd591c4
MrGorlenko <52908127+MrGorlenko@users.noreply.github.com>
2020-05-16 16:25:38 +0300

    menu opacity

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,11 +3,15 @@ $(document).ready(function(){
     $('.header__burger').on('click', function(){
 
         $('.menu').addClass('left-0');
+        $('.menu').addClass('opacity-full');
 
     })
 
     $('.menu__close').on('click', function(){
         $('.menu').removeClass('left-0');
+        $('.menu').removeClass('opacity-full');
     })
 
 })
+
+// .opacity-full
