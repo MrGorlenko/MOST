ce57b643c11a9e267faa6637b0c4f5885182f391
MrGorlenko <52908127+MrGorlenko@users.noreply.github.com>
2020-05-21 22:28:47 +0300

    fixing

---
+++ scripts/team.js
@@ -0,0 +1,333 @@
+$(document).ready(function(){
+
+
+    /* CEO */
+
+    $('.blue_ceo').on('mouseover', function(){
+        
+        $('.team__photo_2').addClass('team__opacity')
+        $('.team__photo_3').addClass('team__opacity')
+        $('.team__photo_4').addClass('team__opacity')
+        $('.team__photo_5').addClass('team__opacity')
+        $('.team__photo_6').addClass('team__opacity')
+        $('.team__photo_7').addClass('team__opacity')
+        $('.team__photo_8').addClass('team__opacity')
+        $('.team__photo_9').addClass('team__opacity')
+        $('.team__photo_10').addClass('team__opacity')
+        $('.team__photo_11').addClass('team__opacity')
+        $('.team__photo_12').addClass('team__opacity')
+
+        $('.blue_cto').addClass('team__opacity')
+        $('.blue_advisors').addClass('team__opacity')
+        $('.blue_developers').addClass('team__opacity')
+        $('.blue_pm').addClass('team__opacity')
+        $('.blue_design').addClass('team__opacity')
+        $('.blue_qa').addClass('team__opacity')
+
+    })
+
+    $('.blue_ceo').on('mouseout', function(){
+        
+        $('.team__photo_2').removeClass('team__opacity')
+        $('.team__photo_3').removeClass('team__opacity')
+        $('.team__photo_4').removeClass('team__opacity')
+        $('.team__photo_5').removeClass('team__opacity')
+        $('.team__photo_6').removeClass('team__opacity')
+        $('.team__photo_7').removeClass('team__opacity')
+        $('.team__photo_8').removeClass('team__opacity')
+        $('.team__photo_9').removeClass('team__opacity')
+        $('.team__photo_10').removeClass('team__opacity')
+        $('.team__photo_11').removeClass('team__opacity')
+        $('.team__photo_12').removeClass('team__opacity')
+
+        $('.blue_cto').removeClass('team__opacity')
+        $('.blue_advisors').removeClass('team__opacity')
+        $('.blue_developers').removeClass('team__opacity')
+        $('.blue_pm').removeClass('team__opacity')
+        $('.blue_design').removeClass('team__opacity')
+        $('.blue_qa').removeClass('team__opacity')
+    })
+
+    /* CEO */
+
+
+    /* CTO */
+
+    $('.blue_cto').on('mouseover', function(){
+        
+        $('.team__photo_1').addClass('team__opacity')
+        $('.team__photo_3').addClass('team__opacity')
+        $('.team__photo_4').addClass('team__opacity')
+        $('.team__photo_5').addClass('team__opacity')
+        $('.team__photo_6').addClass('team__opacity')
+        $('.team__photo_7').addClass('team__opacity')
+        $('.team__photo_8').addClass('team__opacity')
+        $('.team__photo_9').addClass('team__opacity')
+        $('.team__photo_10').addClass('team__opacity')
+        $('.team__photo_11').addClass('team__opacity')
+        $('.team__photo_12').addClass('team__opacity')
+
+        $('.blue_ceo').addClass('team__opacity')
+        $('.blue_advisors').addClass('team__opacity')
+        $('.blue_developers').addClass('team__opacity')
+        $('.blue_pm').addClass('team__opacity')
+        $('.blue_design').addClass('team__opacity')
+        $('.blue_qa').addClass('team__opacity')
+    })
+
+    $('.blue_cto').on('mouseout', function(){
+        
+        $('.team__photo_1').removeClass('team__opacity')
+        $('.team__photo_3').removeClass('team__opacity')
+        $('.team__photo_4').removeClass('team__opacity')
+        $('.team__photo_5').removeClass('team__opacity')
+        $('.team__photo_6').removeClass('team__opacity')
+        $('.team__photo_7').removeClass('team__opacity')
+        $('.team__photo_8').removeClass('team__opacity')
+        $('.team__photo_9').removeClass('team__opacity')
+        $('.team__photo_10').removeClass('team__opacity')
+        $('.team__photo_11').removeClass('team__opacity')
+        $('.team__photo_12').removeClass('team__opacity')
+
+        $('.blue_ceo').removeClass('team__opacity')
+        $('.blue_advisors').removeClass('team__opacity')
+        $('.blue_developers').removeClass('team__opacity')
+        $('.blue_pm').removeClass('team__opacity')
+        $('.blue_design').removeClass('team__opacity')
+        $('.blue_qa').removeClass('team__opacity')
+    })
+
+    /* CTO */
+
+    /* DEVELOP */
+
+    $('.blue_developers').on('mouseover', function(){
+        
+        $('.team__photo_1').addClass('team__opacity')
+        $('.team__photo_2').addClass('team__opacity')
+        $('.team__photo_4').addClass('team__opacity')
+        $('.team__photo_7').addClass('team__opacity')
+        $('.team__photo_8').addClass('team__opacity')
+        $('.team__photo_9').addClass('team__opacity')
+        $('.team__photo_10').addClass('team__opacity')
+        $('.team__photo_11').addClass('team__opacity')
+        $('.team__photo_12').addClass('team__opacity')
+
+        $('.blue_ceo').addClass('team__opacity')
+        $('.blue_cto').addClass('team__opacity')
+        $('.blue_advisors').addClass('team__opacity')
+        $('.blue_pm').addClass('team__opacity')
+        $('.blue_design').addClass('team__opacity')
+        $('.blue_qa').addClass('team__opacity')
+    })
+
+    $('.blue_developers').on('mouseout', function(){
+        
+        $('.team__photo_1').removeClass('team__opacity')
+        $('.team__photo_2').removeClass('team__opacity')
+        $('.team__photo_4').removeClass('team__opacity')
+        $('.team__photo_7').removeClass('team__opacity')
+        $('.team__photo_8').removeClass('team__opacity')
+        $('.team__photo_9').removeClass('team__opacity')
+        $('.team__photo_10').removeClass('team__opacity')
+        $('.team__photo_11').removeClass('team__opacity')
+        $('.team__photo_12').removeClass('team__opacity')
+
+        $('.blue_ceo').removeClass('team__opacity')
+        $('.blue_cto').removeClass('team__opacity')
+        $('.blue_advisors').removeClass('team__opacity')
+        $('.blue_pm').removeClass('team__opacity')
+        $('.blue_design').removeClass('team__opacity')
+        $('.blue_qa').removeClass('team__opacity')
+    })
+
+    /* DEVELOP */
+
+    /* ADVISORS */
+
+    $('.blue_advisors').on('mouseover', function(){
+        
+        $('.team__photo_1').addClass('team__opacity')
+        $('.team__photo_2').addClass('team__opacity')
+        $('.team__photo_3').addClass('team__opacity')
+        $('.team__photo_4').addClass('team__opacity')
+        $('.team__photo_5').addClass('team__opacity')
+        $('.team__photo_6').addClass('team__opacity')
+        $('.team__photo_8').addClass('team__opacity')
+        $('.team__photo_9').addClass('team__opacity')
+        $('.team__photo_12').addClass('team__opacity')
+
+        $('.blue_ceo').addClass('team__opacity')
+        $('.blue_cto').addClass('team__opacity')
+        $('.blue_developers').addClass('team__opacity')
+        $('.blue_pm').addClass('team__opacity')
+        $('.blue_design').addClass('team__opacity')
+        $('.blue_qa').addClass('team__opacity')
+    })
+
+    $('.blue_advisors').on('mouseout', function(){
+        
+        $('.team__photo_1').removeClass('team__opacity')
+        $('.team__photo_2').removeClass('team__opacity')
+        $('.team__photo_3').removeClass('team__opacity')
+        $('.team__photo_4').removeClass('team__opacity')
+        $('.team__photo_5').removeClass('team__opacity')
+        $('.team__photo_6').removeClass('team__opacity')
+        $('.team__photo_8').removeClass('team__opacity')
+        $('.team__photo_9').removeClass('team__opacity')
+        $('.team__photo_12').removeClass('team__opacity')
+
+        $('.blue_ceo').removeClass('team__opacity')
+        $('.blue_cto').removeClass('team__opacity')
+        $('.blue_developers').removeClass('team__opacity')
+        $('.blue_pm').removeClass('team__opacity')
+        $('.blue_design').removeClass('team__opacity')
+        $('.blue_qa').removeClass('team__opacity')
+    })
+
+    /* ADVISORS */
+
+    /* DESIGNERS */
+
+    $('.blue_design').on('mouseover', function(){
+ 
+        $('.team__photo_1').addClass('team__opacity')       
+        $('.team__photo_2').addClass('team__opacity')
+        $('.team__photo_3').addClass('team__opacity')
+        $('.team__photo_4').addClass('team__opacity')
+        $('.team__photo_5').addClass('team__opacity')
+        $('.team__photo_6').addClass('team__opacity')
+        $('.team__photo_7').addClass('team__opacity')
+        $('.team__photo_10').addClass('team__opacity')
+        $('.team__photo_11').addClass('team__opacity')
+        $('.team__photo_12').addClass('team__opacity')
+
+        $('.blue_ceo').addClass('team__opacity')
+        $('.blue_cto').addClass('team__opacity')
+        $('.blue_advisors').addClass('team__opacity')
+        $('.blue_developers').addClass('team__opacity')
+        $('.blue_pm').addClass('team__opacity')
+        $('.blue_qa').addClass('team__opacity')
+    })
+
+    $('.blue_design').on('mouseout', function(){
+        
+        $('.team__photo_1').removeClass('team__opacity')       
+        $('.team__photo_2').removeClass('team__opacity')
+        $('.team__photo_3').removeClass('team__opacity')
+        $('.team__photo_4').removeClass('team__opacity')
+        $('.team__photo_5').removeClass('team__opacity')
+        $('.team__photo_6').removeClass('team__opacity')
+        $('.team__photo_7').removeClass('team__opacity')
+        $('.team__photo_10').removeClass('team__opacity')
+        $('.team__photo_11').removeClass('team__opacity')
+        $('.team__photo_12').removeClass('team__opacity')
+
+        $('.blue_ceo').removeClass('team__opacity')
+        $('.blue_cto').removeClass('team__opacity')
+        $('.blue_advisors').removeClass('team__opacity')
+        $('.blue_developers').removeClass('team__opacity')
+        $('.blue_pm').removeClass('team__opacity')
+        $('.blue_qa').removeClass('team__opacity')
+    })
+
+    /* DESIGNERS */
+
+
+    /* PM */
+
+    $('.blue_pm').on('mouseover', function(){
+
+        $('.team__photo_1').addClass('team__opacity')
+        $('.team__photo_2').addClass('team__opacity')
+        $('.team__photo_3').addClass('team__opacity')
+        $('.team__photo_5').addClass('team__opacity')
+        $('.team__photo_6').addClass('team__opacity')
+        $('.team__photo_7').addClass('team__opacity')
+        $('.team__photo_8').addClass('team__opacity')
+        $('.team__photo_9').addClass('team__opacity')
+        $('.team__photo_10').addClass('team__opacity')
+        $('.team__photo_11').addClass('team__opacity')
+        $('.team__photo_12').addClass('team__opacity')
+
+        $('.blue_ceo').addClass('team__opacity')
+        $('.blue_cto').addClass('team__opacity')
+        $('.blue_advisors').addClass('team__opacity')
+        $('.blue_developers').addClass('team__opacity')
+        $('.blue_design').addClass('team__opacity')
+        $('.blue_qa').addClass('team__opacity')
+    })
+
+    $('.blue_pm').on('mouseout', function(){
+        
+        $('.team__photo_1').removeClass('team__opacity')
+        $('.team__photo_2').removeClass('team__opacity')
+        $('.team__photo_3').removeClass('team__opacity')
+        $('.team__photo_5').removeClass('team__opacity')
+        $('.team__photo_6').removeClass('team__opacity')
+        $('.team__photo_7').removeClass('team__opacity')
+        $('.team__photo_8').removeClass('team__opacity')
+        $('.team__photo_9').removeClass('team__opacity')
+        $('.team__photo_10').removeClass('team__opacity')
+        $('.team__photo_11').removeClass('team__opacity')
+        $('.team__photo_12').removeClass('team__opacity')
+
+        $('.blue_ceo').removeClass('team__opacity')
+        $('.blue_cto').removeClass('team__opacity')
+        $('.blue_advisors').removeClass('team__opacity')
+        $('.blue_developers').removeClass('team__opacity')
+        $('.blue_design').removeClass('team__opacity')
+        $('.blue_qa').removeClass('team__opacity')
+    })
+
+    /* PM */
+
+    /* QA */
+
+    $('.blue_qa').on('mouseover', function(){
+
+        $('.team__photo_1').addClass('team__opacity')
+        $('.team__photo_2').addClass('team__opacity')
+        $('.team__photo_3').addClass('team__opacity')
+        $('.team__photo_4').addClass('team__opacity')
+        $('.team__photo_5').addClass('team__opacity')
+        $('.team__photo_6').addClass('team__opacity')
+        $('.team__photo_7').addClass('team__opacity')
+        $('.team__photo_8').addClass('team__opacity')
+        $('.team__photo_9').addClass('team__opacity')
+        $('.team__photo_10').addClass('team__opacity')
+        $('.team__photo_11').addClass('team__opacity')
+
+        $('.blue_ceo').addClass('team__opacity')
+        $('.blue_cto').addClass('team__opacity')
+        $('.blue_advisors').addClass('team__opacity')
+        $('.blue_developers').addClass('team__opacity')
+        $('.blue_pm').addClass('team__opacity')
+        $('.blue_design').addClass('team__opacity')
+    })
+
+    $('.blue_qa').on('mouseout', function(){
+        
+        $('.team__photo_1').removeClass('team__opacity')
+        $('.team__photo_2').removeClass('team__opacity')
+        $('.team__photo_3').removeClass('team__opacity')
+        $('.team__photo_4').removeClass('team__opacity')
+        $('.team__photo_5').removeClass('team__opacity')
+        $('.team__photo_6').removeClass('team__opacity')
+        $('.team__photo_7').removeClass('team__opacity')
+        $('.team__photo_8').removeClass('team__opacity')
+        $('.team__photo_9').removeClass('team__opacity')
+        $('.team__photo_10').removeClass('team__opacity')
+        $('.team__photo_11').removeClass('team__opacity')
+
+        $('.blue_ceo').removeClass('team__opacity')
+        $('.blue_cto').removeClass('team__opacity')
+        $('.blue_advisors').removeClass('team__opacity')
+        $('.blue_developers').removeClass('team__opacity')
+        $('.blue_pm').removeClass('team__opacity')
+        $('.blue_design').removeClass('team__opacity')
+    })
+
+    /* QA */
+
+})
